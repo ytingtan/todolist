acc852ce3b7150e3cca23a29cb661544b59a77be
Ying Ting <e0773786@u.nus.edu>
2022-05-21 14:54:50 +0800

    Display added tasks

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -19,57 +19,42 @@ export default function TaskManager() {
 
 				return (
 								<>
-								<div>
-								<h2>Add Tasks</h2>
-								<form onSubmit={handleSubmit}>
-								<label>
-								Task:
-								<input 
-									style={{ margin: "0 1rem" }} 
-									type="text" 
-									value={addTaskText}
-									onChange={(event) => {setAddTaskText(event.target.value);}}/>
-								</label>
-								<input type="submit" value="Add" />
-								</form>
-								</div>
+									<div>
+										<h2>Add Tasks</h2>
+										<form onSubmit={handleSubmit}>
+											<label>
+												Task:
+												<input 
+												style={{ margin: "0 1rem" }} 
+												type="text" 
+												value={addTaskText}
+												onChange={(event) => {setAddTaskText(event.target.value);}}/>
+											</label>
+											<input type="submit" value="Add" />
+										</form>
+										</div>
+
 								<div>
 								<h2>Task List</h2>
 								<table style={{ margin: "0 auto", width: "100%" }}>
-								<thead>
-								<tr>
-								<th>No.</th>
-								<th>Task</th>
-								<th>Completed</th>
-								</tr>
-								</thead>
-								<tbody>
-								<tr>
-								<td>1</td>
-								<td>Brew my morning coffee</td>
-								<td>
-								<input type="checkbox" checked={true} />
-								</td>
-								</tr>
-								</tbody>
-								<tbody>
-								<tr>
-								<td>2</td>
-								<td>Task 2</td>
-								<td>
-								<input type="checkbox" checked={true} />
-								</td>
-								</tr>
-								</tbody>
-								<tbody>
-								<tr>
-								<td>3</td>
-								<td>Task 3</td>
-								<td>
-								<input type="checkbox" checked={false} />
-								</td>
-								</tr>
-								</tbody>
+									<thead>
+										<tr>
+											<th>No.</th>
+											<th>Task</th>
+											<th>Completed</th>
+										</tr>
+									</thead>
+									<tbody>
+								    {tasks.map((task, idx) => (
+										<tr>
+											<td>{idx + 1}</td>
+											<td>{task.description}</td>
+											<td>
+												<input type="checkbox" checked={task.isComplete} />
+											</td>
+										</tr>
+										))}
+									</tbody>
 								</table>
 								<p>
 								Add 2 more items of your choosing to the list, so that this looks
